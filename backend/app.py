29b9f6898c1e6211aa6c9e2cd8f16b2fa9dfaf8a
Esha Eman <eshaarid5@gmail.com>
2025-06-21 23:44:51 -0700

    Updated

--- backend/app.py
+++ backend/app.py
@@ -518,11 +518,17 @@ app.register_blueprint(feedback_bp)
 app.register_blueprint(predict_bp)
 @app.after_request
 def add_cors_headers(response):
+    origin = request.headers.get("Origin")
+    allowed_origins = ["http://localhost:3000", "https://insta-guard-eight.vercel.app"]
+
+    if origin in allowed_origins:
+        response.headers["Access-Control-Allow-Origin"] = origin
+
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
     response.headers["Access-Control-Allow-Credentials"] = "true"
     return response
+
 
 # Run the app
 if __name__ == "__main__":
